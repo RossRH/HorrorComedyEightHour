42af72a3a301357f3d62f740f3cf215acf9bc59f
Denzil Buchner <denzilb55@gmail.com>
2022-02-19 18:18:37 +0200

    Add flickering light to reception

---
+++ Assets/FlickerLight.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using LightVolume;
+using UnityEngine;
+
+[RequireComponent(typeof(Light2D))]
+public class FlickerLight : MonoBehaviour
+{
+
+    private Light2D _light;
+
+    private float _currentIntensity, _lastIntensity;
+    private float flickerTime = 0;
+    public float flickerInterval = 0.25f;
+    public float minIntensity, maxIntensity;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _light = GetComponent<Light2D>();
+        _currentIntensity = _light.Intensity;
+        _lastIntensity = _currentIntensity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        flickerTime += Time.deltaTime;
+
+        if (flickerTime >= flickerInterval) {
+            _lastIntensity = _currentIntensity;
+            _currentIntensity = Random.Range(minIntensity, maxIntensity);
+            flickerTime = 0;
+        }
+        
+        _light.Intensity = Mathf.Lerp (_lastIntensity, _currentIntensity, flickerTime / flickerInterval);
+    }
+    
+
+    
+		
+    // Update is called once per frame
+}
--- Assets/Scripts/Light.cs
+++ Assets/Scripts/Light.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using LightVolume;
 using UnityEngine;
 
 public class Light : MonoBehaviour
